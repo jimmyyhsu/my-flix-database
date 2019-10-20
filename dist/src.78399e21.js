dab956c9800ce732be706da31e43e165de8dbdd4
Jimmy Hsu <jimmyyhsu@gmail.com>
2019-10-20 14:49:40 -0700

    Created main-view.jsx

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -32142,7 +32142,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50472" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50458" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
