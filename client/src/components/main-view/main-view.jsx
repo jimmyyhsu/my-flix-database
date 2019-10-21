bf3ae09121d0d93c6e7eb431d43b4457bfeffcc0
Jimmy Hsu <jimmyyhsu@gmail.com>
2019-10-21 14:56:41 -0700

    Updated main-view, movie-view, and movie-card

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export class MainView extends React.Component {
     constructor() {
-        // Call the superclass constructor so React can initialize it
+        // Call the superclass constructor
+        // so React can initialize it
         super();
+
         // Initialize the state to an empty object so we can destructure it later
-        this.state = {};
+        this.state = {
+            movies: null,
+            selectedMovie: null
+        };
     }
 
     // One of the "hooks" available in a React Component
     componentDidMount() {
-        axios.get('<my-api-endpoint/movies>')
+        axios
+            .get("https://myflixdb-api.herokuapp.com/movies")
             .then(response => {
                 // Assign the result to the state
                 this.setState({
@@ -23,21 +32,34 @@ export class MainView extends React.Component {
             });
     }
 
-    // This overrides the render() method of the supperclass
-    // No need to call super() though, as it does nothing by default
+    onMovieClick(movie) {
+        this.setState({
+            selectedMovie: movie
+        });
+    }
+
     render() {
-        // If the state isn't initialized, this will throw on runtime
-        // before the data is initially loaded
-        const { movies } = this.state;
+        const { movies, selectedMovie } = this.state;
 
         // Before the movies have been loaded
         if (!movies) return <div className="main-view" />;
 
         return (
             <div className="main-view">
-                {movies.map(movie => (
-                    <div className="movie-card" key={movie._id}>{movie.Title}</div>
-                ))}
+                {selectedMovie ? (
+                    <MovieView
+                        movie={selectedMovie}
+                        onClick={() => this.onMovieClick()}
+                    />
+                ) : (
+                        movies.map(movie => (
+                            <MovieCard
+                                key={movie._id}
+                                movie={movie}
+                                onClick={movie => this.onMovieClick(movie)}
+                            />
+                        ))
+                    )}
             </div>
         );
     }
--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(function (err, req, res, next) {
   next();
 });
 
-// Homepage for myFlix
+// Homepage
 
 app.get('/', (req, res) => {
   res.send("Welcome to myFlix!")
